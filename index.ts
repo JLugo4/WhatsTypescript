ebf11270ec0df869cd33b6bac2aa5feb5b9b0da5
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-04-16 14:25:10 -0400

    Work in progress: personalizing background/layout

--- index.ts
+++ index.ts
@@ -1,15 +1,33 @@
-// Import stylesheets
-import './style.css';
+//require("./style.css");
+const form = document.querySelector<HTMLFormElement>('#defineform')!;
+const list = document.querySelector<HTMLUListElement>('.list-unstyled')!;
+const header = document.querySelector<HTMLHeadingElement>('h1')!;
 
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-const form: HTMLFormElement = document.querySelector('#defineform');
-
-
-form.onsubmit = () => {
-  const formData = new FormData(form);
-
-  console.log(formData);
+  const formData = new FormData(event.target as HTMLFormElement);
   const text = formData.get('defineword') as string;
-  console.log(text);
-  return false; // prevent reload
-};
+
+  try {
+    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`);
+    const data = await response.json();
+
+    header.textContent = text;
+
+    list.innerHTML = '';
+    data[0].meanings.forEach(({ partOfSpeech, definitions }) => {
+      const li = document.createElement('li');
+      li.textContent = `${partOfSpeech} - ${definitions[0].definition}`;
+      list.appendChild(li);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
+
+
+
+
